93983ffb30a15f42bcf1058a15e6edc696af6eb0
Scott Hoisington <hoyzington@gmail.com>
2020-06-16 17:05:38 -0400

    migrate db, complete routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,27 @@
 Rails.application.routes.draw do
-  resources :courses
-  resources :newbies
-  resources :experts
+
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'apply', to: 'experts#new'
+  post 'apply', to: 'experts#create'
+  resources :experts, except: [:new, :create] do
+    resources :courses, except: [:show]
+  end
+
+  resources :courses, only: [:show] do
+    resources :experts, only: [:index]
+    resources :newbies, only: [:index]
+  end
+
+  get 'signup', to: 'newbies#new'
+  post 'signup', to: 'newbies#create'
+  resources :newbies, except: [:new, :create, :index]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
