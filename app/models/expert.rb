72249dfaea24326955d7f93fc112f86af40f180b
Scott Hoisington <hoyzington@gmail.com>
2020-06-15 20:39:22 -0400

    complete associations, bcrypt

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -1,2 +1,5 @@
 class Expert < ApplicationRecord
+  has_many :courses
+  has_many :newbies, through: :courses
+  has_secure_password
 end
--- app/models/newbie.rb
+++ app/models/newbie.rb
@@ -1,2 +1,5 @@
 class Newbie < ApplicationRecord
+  has_many :courses
+  has_many :experts, through: :courses
+  has_secure_password
 end
