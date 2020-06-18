5545c2077cb1cf69bb59f20487acfe5c649bc5d0
Scott Hoisington <hoyzington@gmail.com>
2020-06-18 15:59:49 -0400

    fix Courses nav link, run OfferedCourse migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_235626) do
+ActiveRecord::Schema.define(version: 2020_06_18_185752) do
 
   create_table "experts", force: :cascade do |t|
     t.string "first_name"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2020_06_15_235626) do
     t.string "last_name"
     t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "offered_courses", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "expert_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
